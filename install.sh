92219df56c78c5dddf234b86f30ae31412855262
Burak T <75041882+CilekciGs@users.noreply.github.com>
2024-01-31 21:14:07 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
 #! /bin/bash
-
 echo "Please run the script in the repo folder"
 echo "Are you sure that the required dependencies are installed?"
 echo ""
@@ -11,37 +10,35 @@ sleep 5
 
 echo "## Copying files ##" 
 mkdir ~/.config
-mkdir ~/.local
-mkdir ~/.local/share
-mkdir ~/.local/share/fonts
 cp -r ./Config/* ~/.config/
 chmod +x ~/.config/bspwm/bspwmrc
 chmod +x ~/.config/rofi/PowerMenu/powermenu.sh
 chmod +x ~/.config/rofi/Brightness/brightness.sh
 chmod +x ~/.config/rofi/Launcher/launcher.sh
 chmod +x ~/.config/rofi/Volume/volume.sh
-doas cp -r ./Wallpapers/* /usr/share/backgrounds
+doas mkdir /usr/share/backgrounds
+doas cp -r ./Wallpapers/* /usr/share/backgrounds/
 doas cp lightdm-gtk-greeter.conf /etc/lightdm/lightdm-gtk-greeter.conf
 
 echo "## Installing fonts ##"
-cp -r ./fonts/* ~/.local/share/fonts/
+mkdir ~/.local
+mkdir ~/.local/share
+mkdir ~/.local/share/fonts
+cp -r ./Fonts/* ~/.local/share/fonts/
 fc-cache -fv
 
-#echo "Installing OhMyFish and Bobthefish theme"
+#echo "## Installing OhMyFish and Bobthefish theme ##"
 #curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
 #fish -c"omf install bobthefish"
 
-echo ""
-
-echo "Installing Kora icons"
+echo "## Installing Kora icons ##"
 git clone https://github.com/bikass/kora.git
 cd kora
 mkdir /usr/share/icons
 doas cp -r ./Nordic-darker.tar.xz /usr/share/icons 
 cd ..
 
-echo "Installing Nordic Theme"
-
+echo "## Installing Nordic Theme ##"
 wget https://github.com/EliverLara/Nordic/releases/download/v2.2.0/Nordic-darker.tar.xz
 tar -xf ./Nordic-darker.tar.xz
 mkdir /usr/share/themes
