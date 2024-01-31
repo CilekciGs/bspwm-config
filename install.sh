209255d27b5f136f1eeecebbd488968204039a57
Burak T <75041882+CilekciGs@users.noreply.github.com>
2024-01-31 19:09:49 +0300

    Fix install.sh

--- install.sh
+++ install.sh
@@ -9,8 +9,10 @@ echo ""
 echo "Starting in 5 Seconds"
 sleep 5
 
-echo "Copying files" 
+echo "## Copying files ##" 
 mkdir ~/.config
+mkdir ~/.local
+mkdir ~/.local/share
 mkdir ~/.local/share/fonts
 cp -r ./Config/* ~/.config/
 chmod +x ~/.config/bspwm/bspwmrc
@@ -19,32 +21,30 @@ chmod +x ~/.config/rofi/Brightness/brightness.sh
 chmod +x ~/.config/rofi/Launcher/launcher.sh
 chmod +x ~/.config/rofi/Volume/volume.sh
 doas cp -r ./Wallpapers/* /usr/share/backgrounds
-sudo cp -r ./Wallpapers/* /usr/share/backgrounds
-cp -r ./fonts/* ~/.local/share/fonts/
-sudo cp lightdm-gtk-greeter.conf /etc/lightdm/lightdm-gtk-greeter.conf
 doas cp lightdm-gtk-greeter.conf /etc/lightdm/lightdm-gtk-greeter.conf
 
-echo "Regenarating Font cache"
+echo "## Installing fonts ##"
+cp -r ./fonts/* ~/.local/share/fonts/
 fc-cache -fv
 
-echo "Installing OhMyFish and Bobthefish theme"
-curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
-fish -c"omf install bobthefish"
+#echo "Installing OhMyFish and Bobthefish theme"
+#curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
+#fish -c"omf install bobthefish"
 
 echo ""
 
 echo "Installing Kora icons"
 git clone https://github.com/bikass/kora.git
 cd kora
-mkdir /usr/share/themes
-sudo cp -r ./kora /usr/share/themes
-doas cp -r ./kora /usr/share/themes
-
-echo "Installing Nordic Theme"
-cd ..
-curl -O https://github.com/EliverLara/Nordic/releases/download/v2.2.0/Nordic-darker.tar.xz
-tar -xf ./Nordic-darker.tar.xz
 mkdir /usr/share/icons
 doas cp -r ./Nordic-darker.tar.xz /usr/share/icons 
+cd ..
+
+echo "Installing Nordic Theme"
+
+wget https://github.com/EliverLara/Nordic/releases/download/v2.2.0/Nordic-darker.tar.xz
+tar -xf ./Nordic-darker.tar.xz
+mkdir /usr/share/themes
+doas cp -r ./Nordic-darker /usr/share/themes
 
 echo "Done! Now set themes with lxapperance!"
