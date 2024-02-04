032df9edd1a838de4120637c2922f6a958311eea
Burak T <75041882+CilekciGs@users.noreply.github.com>
2024-02-04 16:55:56 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,29 +1,48 @@
 #! /bin/bash
+elevate=sudo
+if [ -e "/usr/bin/doas" ]; then
+	elevate=doas
+fi
+
 echo "Please run the script in the repo folder"
 echo "Are you sure that the required dependencies are installed?"
 echo ""
 cat deps.txt
+
+function CreateIfNotExist
+{
+	if [ ! -d $1 ]; then
+		mkdir $1
+	fi
+}
+
+function elevateCreateIfNotExist
+{
+	if [ ! -d $1 ]; then
+		$elevate mkdir $1
+	fi
+}
 
 echo ""
 echo "Starting in 5 Seconds"
 sleep 5
 
 echo "## Copying files ##" 
-mkdir ~/.config
+CreateIfNotExist ~/.config
 cp -r ./Config/* ~/.config/
 chmod +x ~/.config/bspwm/bspwmrc
 chmod +x ~/.config/rofi/PowerMenu/powermenu.sh
 chmod +x ~/.config/rofi/Brightness/brightness.sh
 chmod +x ~/.config/rofi/Launcher/launcher.sh
 chmod +x ~/.config/rofi/Volume/volume.sh
-doas mkdir /usr/share/backgrounds
-doas cp -r ./Wallpapers/* /usr/share/backgrounds/
-doas cp lightdm-gtk-greeter.conf /etc/lightdm/lightdm-gtk-greeter.conf
+elevateCreateIfNotExist /usr/share/backgrounds
+$elevate cp -r ./Wallpapers/* /usr/share/backgrounds/
+$elevate cp lightdm-gtk-greeter.conf /etc/lightdm/lightdm-gtk-greeter.conf
 
 echo "## Installing fonts ##"
-mkdir ~/.local
-mkdir ~/.local/share
-mkdir ~/.local/share/fonts
+CreateIfNotExist ~/.local
+CreateIfNotExist ~/.local/share
+CreateIfNotExist ~/.local/share/fonts
 cp -r ./Fonts/* ~/.local/share/fonts/
 fc-cache -fv
 
@@ -34,14 +53,14 @@ fc-cache -fv
 echo "## Installing Kora icons ##"
 git clone https://github.com/bikass/kora.git
 cd kora
-mkdir /usr/share/icons
-doas cp -r ./Nordic-darker.tar.xz /usr/share/icons 
+elevateCreateIfNotExist /usr/share/icons
+$elevate cp -r ./Nordic-darker.tar.xz /usr/share/icons 
 cd ..
 
 echo "## Installing Nordic Theme ##"
 wget https://github.com/EliverLara/Nordic/releases/download/v2.2.0/Nordic-darker.tar.xz
 tar -xf ./Nordic-darker.tar.xz
-mkdir /usr/share/themes
-doas cp -r ./Nordic-darker /usr/share/themes
+elevateCreateIfNotExist /usr/share/themes
+$elevate cp -r ./Nordic-darker /usr/share/themes
 
 echo "Done! Now set themes with lxapperance!"
